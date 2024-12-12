4e300d22289f3e2952e8283634509c85dafcd990
gerworkspaces <quang.vo.ielts@gmail.com>
2024-12-12 15:55:27 +0700

    feat: event page ui

---
+++ components/ReviewCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="bg-[#EFEDE0] py-8 px-8 flex flex-col">
+    <div class="container mx-auto px-48">
+      <div class="text-yellow-500 flex space-x-1 text-3xl">
+        <!-- Hiển thị 5 ngôi sao -->
+        <span v-for="i in 5" :key="i">★</span>
+      </div>
+      <h2 class="text-black text-xl font-semibold mt-4">
+        "Multi Coloured Art Theme"
+      </h2>
+      <p class="text-gray-500 mt-2">Wood and glass finishes</p>
+
+      <div class="flex items-center mt-6 space-x-4">
+        <img
+          :src="avtImg"
+          alt="Reviewer avatar"
+          class="w-12 h-12 rounded-full object-cover"
+        />
+        <div class="text-left">
+          <p class="text-gray-800 font-medium">Quang Vo - Developer</p>
+          <p class="text-gray-500 text-sm">20-11-2024</p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import avtImg from "~/assets/images/my-avt.png";
+</script>
+
+<style scoped>
+/* Tùy chỉnh thêm CSS nếu cần */
+</style>
---
+++ components/VisitorInfo.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="py-10 bg-[#333333] container mx-auto">
+    <div class="flex justify-center px-44">
+      <!-- Image content -->
+      <div class="w-1/2 pr-16">
+        <img
+          :src="visitorInfoImg"
+          alt="Museum exhibit"
+          class="rounded-lg shadow-lg"
+        />
+      </div>
+
+      <!-- Text content -->
+      <div class="w-1/2 text-white pl-16 my-12">
+        <p class="text-yellow-400 text-2xl uppercase tracking-wider font-semibold">
+          Visitor Information
+        </p>
+        <h1 class="text-4xl font-bold mt-4">Welcome to visit and take the time to introduce you to more historical information and knowledge you may enjoy.</h1>
+
+        <div class="mt-8">
+          <h3 class="text-lg font-semibold">Opening Hours</h3>
+          <p class="text-gray-300">
+            Monday - Friday .................. 09:00am - 8:30pm
+          </p>
+          <p class="text-gray-300">
+            Saturday - Sunday ............... 09:00am - 1:30pm
+          </p>
+        </div>
+
+        <div class="mt-8">
+          <h3 class="text-lg font-semibold">Location</h3>
+          <p class="text-gray-300">
+            03 Quang Trung street, Hai Chau district, Danang city, 550000, Vietnam
+            Vietnamese.<br />
+            (+84) 123-456-789<br />
+            museum@system.com
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+import visitorInfoImg from "@/assets/images/visitor-info.png";
+</script>
